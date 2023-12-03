ae6df30aee56e7e773246af4601c62c8b750ea7d
Brian Qiu <brianlqiu5@gmail.com>
2023-12-02 23:30:52 -0800

    day1 pt 1 solution

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let res = io::stdin()
+        .lines()
+        .map(|line| {
+            let line = line.expect("failed to read line");
+            line.chars()
+                .filter_map(|c| c.to_digit(/*radix=*/ 10))
+                .nth(0)
+                .expect("line has no digit")
+                * 10
+                + line
+                    .chars()
+                    .rev()
+                    .filter_map(|c| c.to_digit(/*radix=*/ 10))
+                    .nth(0)
+                    .unwrap()
+        })
+        .sum::<u32>();
+    println!("{}", res);
 }
